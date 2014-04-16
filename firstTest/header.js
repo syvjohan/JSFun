a98d990d1717e759800059ffee401a62000f0b7a
syvjohan <johanfredriksson1987@gmail.com>
2014-04-16 18:24:09 +0200

    first push

---
+++ firstTest/header.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>testing js</title>
+		<meta charset="utf-8" name="Johan testing js" content="version 1.0">
+	</head>
+	<body>
+		<div>
+			<h1>Välkommen till testet</h1>
+		</div>
+		<div>
+			<h2>Här kommer bildspelet</h2>
+		</div>
+		<div>
+			<h3>Här kommer lite andra effekter</h3>
+		</div>
+	</body>
+</html>
