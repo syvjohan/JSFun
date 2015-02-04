f3349efff6441efe7cd80a395ddd2a495574fe6a
syvjohan <johanfredriksson1987@gmail.com>
2015-02-04 22:38:12 +0100

    setting up basic structure

---
+++ structure/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang='en' class='no-js'>
+<head>
+	<meta charset='utf-8' />
+	<title><?=isset($title) ? $title : 'Tempalte for testprogram in Javascript'?></title>
+	<link rel="stylesheet/less" type="text/css" href="style.less">
+	<script src="/../structure/js-libb/less.js-2.3.1/dist/less.min.js"</script>
+	<script src="/../structure/js-libb/modernizr.custom.55587.js"
+</head>
+<body>
---
+++ structure/index.php
@@ -0,0 +1,7 @@
+<?php $title='Template for testprogram'; include(_DIR_ . '/../structure/header.php'); ?>
+
+<div id='flash'>
+	<p id='text' class='red'>Hi this text should be replaced when page and DOM is loaded.</p>
+</div>
+
+<?php $path=_DIR_; include(_DIR_ . '/../structure/footer.php'); ?>
